c21f50380e08a10c7c09ebb80a975d635a5e80a7
whandell <whandellg@gmail.com>
2022-08-24 17:16:54 -0300

    cards procediments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'header', component: HeaderComponent}
 
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
