9a9f3d761fc05f5173ed9aab13bf55ff8df630f9
whandell <whandellg@gmail.com>
2022-08-30 19:27:03 -0300

    Merge branch 'main' into develop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProcedureComponent } from './procedure/procedure.component';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'underconstruction' , component: UnderConstructionComponent },
+  { path: 'procedure' , component: ProcedureComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
